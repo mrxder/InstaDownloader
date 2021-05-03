18eba9990c46590afff1f154a6080e4f23c4062e
Matthias Keim <info@matthias-keim.com>
2021-05-03 19:11:52 +0200

    ini

---
+++ background.js
@@ -0,0 +1,71 @@
+// Inject
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (
+        changeInfo.status == 'complete' &&
+        tab.url.startsWith('https://www.instagram.com')
+    ) {
+        chrome.tabs.executeScript(tabId, { file: 'foreground.js' }, () => {
+            //   console.log("injected");
+        });
+        chrome.tabs.insertCSS(tabId, { file: 'insta.css' });
+    }
+});
+
+// Downloader
+const processDownloadMessage = (msg) => {
+    if (msg.success) {
+        const urlObj = new URL(msg.url);
+        const urlPathArray = urlObj.pathname.split('/');
+        const fileName = urlPathArray[urlPathArray.length - 1];
+        console.log(msg.username);
+
+        chrome.downloads.download({
+            url: msg.url,
+            filename: msg.username + '_' + fileName,
+        });
+    } else {
+        console.log(msg);
+    }
+};
+
+// Story
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request) {
+        processDownloadMessage(request);
+    }
+});
+
+// Normal
+
+const idImg = chrome.contextMenus.create({
+    title: 'Download this Img',
+    contexts: ['all'],
+    id: 'downloadImg',
+});
+
+const idVid = chrome.contextMenus.create({
+    title: 'Download this Video',
+    contexts: ['all'],
+    id: 'downloadVideo',
+});
+
+chrome.contextMenus.onClicked.addListener((menuItem, tab) => {
+    if (
+        menuItem.pageUrl === 'https://www.instagram.com/' ||
+        menuItem.pageUrl.startsWith('https://www.instagram.com/p/')
+    ) {
+        let target = undefined;
+        if (menuItem.menuItemId === 'downloadImg') {
+            target = 'img';
+        } else if (menuItem.menuItemId === 'downloadVideo') {
+            target = 'video';
+        }
+
+        if (target) {
+            chrome.tabs.sendMessage(tab.id, { target: target }, (resp) => {
+                processDownloadMessage(resp);
+            });
+        }
+    }
+});
---
+++ foreground.js
@@ -0,0 +1,178 @@
+window.oncontextmenu = null;
+
+if (typeof hasAlreadyBeenDecleared === 'undefined') {
+    const hasAlreadyBeenDecleared = true;
+    // Stories
+    const getUserNameOfStory = () => {
+        const hrefOfProfilePic = document
+            .querySelector('header a')
+            .getAttribute('href');
+        const userName = hrefOfProfilePic.substring(
+            1,
+            hrefOfProfilePic.length - 1
+        );
+
+        return userName;
+    };
+    const getImgUrlFromStory = (imgNode) => {
+        const srcArray = imgNode.attributes['srcset'].textContent.split(' ');
+        const srcToDownload = srcArray[0];
+        return srcToDownload;
+    };
+
+    const getVideoUrlFromStory = (videoNode) => {
+        return videoNode.childNodes[0].attributes['src'].textContent;
+    };
+
+    const clickOnDownloadOriginalStory = () => {
+        const imgObj = document.querySelector('section section img');
+        const videoObj = document.querySelector('section section video');
+        const userName = getUserNameOfStory();
+
+        if (videoObj) {
+            const videoUrl = getVideoUrlFromStory(videoObj);
+
+            chrome.runtime.sendMessage({
+                success: true,
+                url: videoUrl,
+                username: userName,
+            });
+        } else if (imgObj) {
+            const imgUrl = getImgUrlFromStory(imgObj);
+
+            chrome.runtime.sendMessage({
+                success: true,
+                url: imgUrl,
+                username: userName,
+            });
+        }
+    };
+
+    const clickOnDownloadImgStory = () => {
+        const imgObj = document.querySelector('section section img');
+        const userName = getUserNameOfStory();
+
+        if (imgObj) {
+            const imgUrl = getImgUrlFromStory(imgObj);
+
+            chrome.runtime.sendMessage({
+                success: true,
+                url: imgUrl,
+                username: userName,
+            });
+        }
+    };
+
+    document.addEventListener('click', (e) => {
+        setTimeout(() => {
+            const oldDownOrigBtn = document.getElementById('story-down-orig');
+            const oldDownImgBtn = document.getElementById('story-down-img');
+            if (
+                window.location.href.startsWith(
+                    'https://www.instagram.com/stories/'
+                )
+            ) {
+                // Download Orig
+
+                if (oldDownOrigBtn == null) {
+                    const downloadBtn = document.createElement('BUTTON');
+                    downloadBtn.innerHTML = 'Download Orig';
+                    downloadBtn.id = 'story-down-orig';
+                    downloadBtn.onclick = clickOnDownloadOriginalStory;
+                    document.body.appendChild(downloadBtn);
+                }
+
+                // Download Img
+
+                if (oldDownImgBtn == null) {
+                    const downloadBtn = document.createElement('BUTTON');
+                    downloadBtn.innerHTML = 'Download img';
+                    downloadBtn.id = 'story-down-img';
+                    downloadBtn.onclick = clickOnDownloadImgStory;
+                    document.body.appendChild(downloadBtn);
+                }
+            } else {
+                if (oldDownOrigBtn) {
+                    oldDownOrigBtn.parentNode.removeChild(oldDownOrigBtn);
+                }
+
+                if (oldDownImgBtn) {
+                    oldDownImgBtn.parentNode.removeChild(oldDownImgBtn);
+                }
+            }
+        }, 100);
+    });
+
+    // Normal
+
+    let lastContextMenuClick = undefined;
+
+    document.addEventListener('contextmenu', (e) => {
+        lastContextMenuClick = e.target;
+    });
+
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+        let articleNode = lastContextMenuClick;
+        while (articleNode.nodeName !== 'ARTICLE') {
+            articleNode = articleNode.parentNode;
+        }
+        let usernameObj = articleNode.childNodes[0].childNodes[1].childNodes[0];
+        while (usernameObj.nodeName !== 'A') {
+            usernameObj = usernameObj.childNodes[0];
+        }
+
+        if (request.target === 'img') {
+            const imgNode =
+                lastContextMenuClick.parentNode.childNodes[0].childNodes[0];
+            if (imgNode.nodeName === 'IMG') {
+                const srcArray = imgNode.attributes['srcset'].textContent.split(
+                    ' '
+                );
+                const srcToDownload = srcArray[srcArray.length - 2].split(
+                    ','
+                )[1];
+                sendResponse({
+                    success: true,
+                    url: srcToDownload,
+                    username: usernameObj.innerText,
+                });
+            } else {
+                sendResponse({ success: false });
+            }
+        } else if (request.target === 'video') {
+            const allScriptsArray = document.querySelectorAll('script');
+
+            let tOfScripts = 0;
+            for (; tOfScripts < allScriptsArray.length; tOfScripts++) {
+                if (allScriptsArray[tOfScripts].innerText.includes('.mp4')) {
+                    break;
+                }
+            }
+
+            const innerParts = allScriptsArray[tOfScripts].innerText.split(
+                'BaseURL'
+            );
+
+            let tOfParts = 0;
+            for (; tOfParts < innerParts.length; tOfParts++) {
+                if (innerParts[tOfParts].includes('.mp4')) {
+                    break;
+                }
+            }
+
+            let targetPart = innerParts[tOfParts];
+            targetPart = targetPart.replace(/.u0026amp./gi, '&');
+            const urlStartsAt = targetPart.indexOf('https:');
+            targetPart = targetPart.substring(
+                urlStartsAt,
+                targetPart.length - 7
+            );
+
+            sendResponse({
+                success: true,
+                url: targetPart,
+                username: usernameObj.innerText,
+            });
+        }
+    });
+}
